6acd055b0c7460b5b59672ba64ba130b16188833
ju3tin <ju3tin@hotmail.co.uk>
2023-06-11 06:11:19 +0100

    update

--- js/designesia.js
+++ js/designesia.js
@@ -411,6 +411,27 @@
             }
          });
 
+         jQuery("#collection-carousel-alt2").owlCarousel({
+            center: false,
+            items:5,
+            loop:true,
+            margin:25,
+            nav:true,
+            navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
+            dots:false,
+            responsive:{
+                1000:{
+                    items:5
+                },
+                600:{
+                    items:2
+                },
+                0:{
+                    items:1
+                }
+            }
+         });
+
          jQuery("#item-carousel-big").owlCarousel({
             loop:true,
             margin:20,
